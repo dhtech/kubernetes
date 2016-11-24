b8fa8edd21c6c587161472310d9b9917b5f1587b
Christian Svensson <christian.svensson@tink.se>
2016-11-25 00:23:31 +0100

    updates from production

--- dhtech.sh
+++ dhtech.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+echo "Enter password for docker registry"
+echo -n "Username: "
+read username
+echo -n "Password: "
+read password
+
+kubectl create secret docker-registry tech-registry-key \
+  --docker-server=val.tech.dreamhack.se \
+  --docker-username="${username}" \
+  --docker-password="${password}" \
+  --docker-email=services@tech.dreamhack.se
+kubectl apply -f calico/calico.yaml
+kubectl apply -f svc-bgp/svc-bgp.yaml
+
 kubectl create namespace public
 kubectl create namespace prod
 
-for i in svc/*.yaml
-do
-  kubectl apply -f $i
-done
+kubectl replace -f svc/kube-dns.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/release-1.2/deploy/kube-config/standalone/heapster-controller.yaml
+kubectl apply -f svc/heapster.yaml
+
+kubectl apply -f https://rawgit.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard.yaml
+kubectl replace -f svc/kubernetes-dashboard.yaml
