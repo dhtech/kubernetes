c13ca7467e05539ae83f082df8eae212d64c6a08
Christian Svensson <christian.svensson@tink.se>
2016-11-27 08:28:57 +0100

    save all pod logs as well

--- save-all.sh
+++ save-all.sh
@@ -8,3 +8,13 @@ do
     kubectl get "$thing" --namespace="$ns" -o yaml > "kube-$ns-$thing.yaml"
   done
 done
+
+for ns in $(kubectl get namespace -o name | cut -f 2 -d '/')
+do
+  echo "Saving logs for namespace $ns"
+  for pod in $(kubectl get pods --namespace="$ns" -o name | cut -f 2 -d '/')
+  do
+    kubectl logs --namespace="$ns" $pod > logs-"$ns"-"$pod".log
+    kubectl logs --namespace="$ns" -p $pod > logs-"$ns"-"$pod".prev.log
+  done
+done
