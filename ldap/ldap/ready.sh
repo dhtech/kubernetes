93f296de4835b654f3a83002ee10290888b11dba
Christian Svensson <christian.svensson@tink.se>
2017-06-18 22:38:25 +0200

    ready.sh probe

---
+++ ldap/ldap/ready.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Only look at the integer part of the CSN, should be good enough.
+CSN=$(ldapsearch -x -s base -LLL contextCSN \
+  | awk '/contextCSN:/ {print $2}' | cut -f 1 -d '.')
+INITIAL_CSN=$(</initial-contextCSN | cut -f 1 -d '.')
+
+# If the local contextCSN is greater than or equal to the initial CSN the
+# instance is considered to be in sync.
+[ "${CSN}" -ge "${INITIAL_CSN}" ]
